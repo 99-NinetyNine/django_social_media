7a8815be96fafc46592ebd41410743f2da75876f
heroku <saileshshiwakoti2@gamil.com>
2020-08-26 20:39:07 +0545

    vayo!

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -160,15 +160,12 @@ AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-""" 
-AWS_ACCESS_KEY_ID='AKIAU6IIHOYNO3RPJCVU'
-AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME='salilesh-bucket'
-"""
+
 #unique name for files
 AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com'%AWS_STORAGE_BUCKET_NAME
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
