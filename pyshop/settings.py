4d29295f2084b55ff64e39ccb26345019b3e13b2
heroku <saileshshiwakoti2@gamil.com>
2020-08-26 15:59:02 +0545

    2

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -169,5 +169,6 @@ AWS_STORAGE_BUCKET_NAME='salilesh-bucket'
 AWS_S3_FILE_OVERWRITE=False
 AWS_DEFAULT_ACL=None
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 django_heroku.settings(locals())
