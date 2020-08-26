520a7eb9c521fbaaa328becba96b7f530f785453
heroku <saileshshiwakoti2@gamil.com>
2020-08-26 18:59:46 +0545

    1 try

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','neyture.herokuapp.com',]
 
 LOGIN_REDIRECT_URL = "index_page"
 LOGIN_URL = "login"
