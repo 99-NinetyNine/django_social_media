db5ec247e3b68a44634224c3b387f1162e26c412
heroku <saileshshiwakoti2@gamil.com>
2020-08-27 21:14:55 +0545

    sas

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = (os.environ.get('DEBUG_VALUE')=="True")
 
 ALLOWED_HOSTS = ['127.0.0.1','neyture.herokuapp.com',]
 
