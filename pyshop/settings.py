fe7c00aa7e0279fce9817e8b5e68319205a0302b
asasasdas <58628378+sailesh-122@users.noreply.github.com>
2021-10-21 12:45:12 +0545

    Update settings.py

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True #(os.environ.get('DEBUG_VALUE')=="True")
+DEBUG = (os.environ.get('DEBUG_VALUE')=="True")
 
 ALLOWED_HOSTS = ['127.0.0.1','neyture.herokuapp.com',"www.earthespy.com"]
 
